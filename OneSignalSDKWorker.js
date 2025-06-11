91077c14838723a6d21940dd6b1f40b24fe6c94d
prometheuslum <berardiandrea188@gmail.com>
2025-06-11 10:09:10 +0200

    Add files via upload

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,1 @@
+// Worker file for OneSignal
---
+++ onesignal.js
@@ -0,0 +1,6 @@
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+OneSignalDeferred.push(async function(OneSignal) {
+  await OneSignal.init({
+    appId: "1c543000-40a9-41f3-8363-ae463ede7251",
+  });
+});
