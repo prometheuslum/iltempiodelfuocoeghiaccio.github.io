f5b32c3d2928659ad21a299a7ed162312380292c
prometheuslum <berardiandrea188@gmail.com>
2025-06-11 12:26:31 +0200

    Delete onesignal.js

--- onesignal.js
+++
@@ -1,6 +0,0 @@
-window.OneSignalDeferred = window.OneSignalDeferred || [];
-OneSignalDeferred.push(async function(OneSignal) {
-  await OneSignal.init({
-    appId: "1c543000-40a9-41f3-8363-ae463ede7251",
-  });
-});
